77a41909b030d52dc66ac9eadff06b8c59db47ca
Koichi NOGUCHI <koichi.noguchi@rakuten.com>
2018-09-28 18:04:46 +0900

    Add jmxremote setting

--- recipes/config.rb
+++ recipes/config.rb
@@ -3,6 +3,21 @@
 # Recipe:: config
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+passwords = data_bag_item('passwords', 'jmxremote')
+
+log "jmx_user is #{passwords['jmx_user']}"
+
+template '/etc/cassandra/jmxremote.password' do
+  source 'jmxremote.password.erb'
+  owner  'cassandra'
+  group  'cassandra'
+  mode   '0400'
+  variables(
+    :jmx_user => passwords['jmx_user'],
+    :jmx_pass => passwords['jmx_password']
+  )
+end
+
 template '/etc/cassandra/cassandra.yaml' do
   source 'cassandra.yaml.erb'
   notifies :run, 'execute[apply configuration]', :immediately

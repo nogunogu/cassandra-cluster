6495d3b5436dc462dd2b950f3559ce154698c371
Koichi NOGUCHI <koichi.noguchi@rakuten.com>
2018-10-01 14:56:11 +0900

    add cluster setting

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,8 +5,19 @@ default['java']['jdk_version'] = '8'
 # for cassandra.yaml
 default['cassandra']['seeds'] = 'localhost'
 default['cassandra']['listen_address'] = 'localhost'
-default['cassandra']['datacenter'] = 'DC1'
-
 default['cassandra']['cluster_name'] = 'Test Cluster1'
-default['cassandra']['snitch'] = 'SimpleSnitch'
 default['cassandra']['auto_bootstrap'] = 'true'
+
+
+# for cluster settings
+default['cassandra']['snitch'] = 'SimpleSnitch' #'GossipingPropertyFileSnitch'
+default['cassandra']['datacenter'] = 'DC1'
+default['cassandra']['topology'] = '
+10.146.0.2=DC1:RAC1
+10.146.0.3=DC1:RAC1
+10.146.0.4=DC1:RAC1
+
+10.142.0.2=DC2:RAC1
+10.142.0.3=DC2:RAC1
+10.142.0.4=DC2:RAC1
+'
